9e2e5f2e3bab8592e770038e0705d7cea3a460df
Dmitrii_56 <ilin.dmitrii1984@gmail.com>
2018-12-11 20:58:04 +0300

    Перенес в отдельный исполняемый файл тесты, подключение модулей. В файле модуля осталась функция сложения связанных списков, в дополнении к шаблону

---
+++ LL.spec
@@ -0,0 +1,36 @@
+# -*- mode: python -*-
+
+block_cipher = None
+
+
+a = Analysis(['LL.py'],
+             pathex=['C:\\Users\\Дмитрий\\AppData\\Roaming\\Microsoft\\Windows\\Start Menu\\Programs\\Python 3.7\\Программы\\Python\\Lesson_1(Linked List in template)'],
+             binaries=[],
+             datas=[],
+             hiddenimports=[],
+             hookspath=[],
+             runtime_hooks=[],
+             excludes=[],
+             win_no_prefer_redirects=False,
+             win_private_assemblies=False,
+             cipher=block_cipher,
+             noarchive=False)
+pyz = PYZ(a.pure, a.zipped_data,
+             cipher=block_cipher)
+exe = EXE(pyz,
+          a.scripts,
+          [],
+          exclude_binaries=True,
+          name='LL',
+          debug=False,
+          bootloader_ignore_signals=False,
+          strip=False,
+          upx=True,
+          console=True )
+coll = COLLECT(exe,
+               a.binaries,
+               a.zipfiles,
+               a.datas,
+               strip=False,
+               upx=True,
+               name='LL')
--- LL_mod.py
+++ LL_mod.py
@@ -1,7 +1,6 @@
 """
 Реализация класса Linked List и Node в шаблоне
 """
-import random, unittest
 class Node:
 
     def __init__(self, v):
@@ -39,7 +38,7 @@ class LinkedList:
             node = node.next
         return None
 
-    def clean_list(self):
+    def clean(self):
         # Удаление всех элементов LL
         node=self.head
         while node is not None:
@@ -136,150 +135,3 @@ def add_linked_lists(list_1,list_2):
         return None
 
     
-def data_for_tests_one(self,randomnumber):
-    one=Node(randomnumber)
-    s_list=LinkedList()
-    s_list.add_in_tail(one)
-    return s_list
-
-def random_generation_number():
-    number=random.randint(1,10**9)
-    return int(number) 
-
-def List_maker(number):
-    Value_for_LL=[]
-    for i in range(0,number):
-        Value_for_LL.append(random_generation_number())
-    return Value_for_LL
-
-class Linked_List_2_Tests(unittest.TestCase):
-   
-    def test_zero(self):
-        #Тест проверяет связанный список с нулем элементов
-        number=1
-        list_for_first_test=List_maker(number)
-        self.Linked_List_for_zero_test=LinkedList()
-        for i in range(0,len(list_for_first_test)):
-            self.Linked_List_for_zero_test.add_in_tail(Node(list_for_first_test[i]))
-        node_1=self.Linked_List_for_zero_test.head
-        for i in range(0,self.Linked_List_for_zero_test.lenght_list()):
-            print(node_1.value)
-            self.assertEqual(list_for_first_test[i],node_1.value)
-            node_1=node_1.next
-
-    def test_one(self):
-        #Тест для связанного списка из 1 эл-та
-        number=2
-        index_for_delete=random.randint(0,number)
-        value_for_insert=random_generation_number()
-        position_for_insert=1
-        list_for_second_test=List_maker(number)        
-        self.Linked_List_for_second_test=LinkedList()
-        #Добавление эл-та в конец 
-        for i in range(0,len(list_for_second_test)):
-            self.Linked_List_for_second_test.add_in_tail(Node(list_for_second_test[i]))
-        node_2=self.Linked_List_for_second_test.head
-        for i in range(0,self.Linked_List_for_second_test.lenght_list()):
-            print(node_2.value)
-            self.assertEqual(list_for_second_test[i],node_2.value)
-            node_2=node_2.next
-        print(list_for_second_test)
-        #Вставка элемента
-        list_for_second_test.insert(position_for_insert,value_for_insert)
-        self.Linked_List_for_second_test.insert_node(position_for_insert,value_for_insert)
-        node_2=self.Linked_List_for_second_test.head
-        for i in range(0,self.Linked_List_for_second_test.lenght_list()):
-            print(node_2.value)
-            self.assertEqual(list_for_second_test[i],node_2.value)
-            node_2=node_2.next
-        print(list_for_second_test)
-        #Поиск элемента который вставили
-        find_node=self.Linked_List_for_second_test.find(value_for_insert)
-        self.assertEqual(find_node.value,value_for_insert)
-        #Поиск элемента по значению
-        index_for_find=random.randint(0,number)
-        value_for_find=list_for_second_test[index_for_find]
-        find_element=self.Linked_List_for_second_test.find(value_for_find)
-        self.assertEqual(value_for_find,find_element.value)
-        #Удаление элемента
-        value_for_delete=list_for_second_test[index_for_delete]
-        list_for_second_test.remove(value_for_delete)
-        print(list_for_second_test)
-        self.Linked_List_for_second_test.del_node(value_for_delete)
-        self.Linked_List_for_second_test.print_all_nodes()
-        node_2=self.Linked_List_for_second_test.head
-        print(self.Linked_List_for_second_test.lenght_list())
-        for i in range(0,self.Linked_List_for_second_test.lenght_list()):
-            self.assertEqual(list_for_second_test[i],node_2.value)
-            node_2=node_2.next
-
-    def test_big_LL(self):
-        #Тест для большого кол-ва элементов
-        number=random.randint(10,30)
-        index_for_delete=random.randint(0,number)
-        value_for_insert=random_generation_number()
-        position_for_insert=random.randint(0,number)
-        list_for_third_test=List_maker(number)        
-        self.Linked_List_for_third_test=LinkedList()
-        #Добавление эл-та в конец
-        for i in range(0,len(list_for_third_test)):
-            self.Linked_List_for_third_test.add_in_tail(Node(list_for_third_test[i]))
-        node_3=self.Linked_List_for_third_test.head
-        for i in range(0,self.Linked_List_for_third_test.lenght_list()):
-            print(node_3.value)
-            self.assertEqual(list_for_third_test[i],node_3.value)
-            node_3=node_3.next
-        #Вставка элемента
-        list_for_third_test.insert(position_for_insert,value_for_insert)
-        self.Linked_List_for_third_test.insert_node(position_for_insert,value_for_insert)
-        node_3=self.Linked_List_for_third_test.head
-        for i in range(0,self.Linked_List_for_third_test.lenght_list()):
-            print(node_3.value)
-            self.assertEqual(list_for_third_test[i],node_3.value)
-            node_3=node_3.next
-        print(list_for_third_test)
-        #Поиск элемента который вставили
-        find_node=self.Linked_List_for_third_test.find(value_for_insert)
-        print(find_node.value,value_for_insert)
-        self.assertEqual(find_node.value,value_for_insert) 
-        #Поиск элемента по значению
-        index_for_find=random.randint(0,number)
-        value_for_find=list_for_third_test[index_for_find]
-        find_element=self.Linked_List_for_third_test.find(value_for_find)
-        self.assertEqual(value_for_find,find_element.value)
-        #Удаление элемента
-        value_for_delete=list_for_third_test[index_for_delete]
-        list_for_third_test.remove(value_for_delete)
-        print(list_for_third_test)
-        self.Linked_List_for_third_test.del_node(value_for_delete)
-        self.Linked_List_for_third_test.print_all_nodes()
-        node_3=self.Linked_List_for_third_test.head
-        print(self.Linked_List_for_third_test.lenght_list())
-        for i in range(0,self.Linked_List_for_third_test.lenght_list()):
-            self.assertEqual(list_for_third_test[i],node_3.value)
-            node_3=node_3.next
-        #Сложение двух списков
-        self.Linked_List_for_third_1_test=LinkedList()
-        self.Linked_List_for_third_2_test=LinkedList()
-        list_for_third_1_test=List_maker(number) 
-        list_for_third_2_test=List_maker(number)
-        sum_list=[] 
-        for i in range(0,len(list_for_third_1_test)):
-            sum_list.append(list_for_third_1_test[i]+list_for_third_2_test[i])
-        for i in range(0,len(list_for_third_1_test)):
-            self.Linked_List_for_third_1_test.add_in_tail(Node(list_for_third_1_test[i]))
-        for i in range(0,len(list_for_third_2_test)):
-            self.Linked_List_for_third_2_test.add_in_tail(Node(list_for_third_2_test[i]))
-        Sum_LL=add_linked_lists(self.Linked_List_for_third_1_test,self.Linked_List_for_third_2_test)
-        Sum_Node=Sum_LL.head
-        for i in range(0,Sum_LL.lenght_list()):
-            self.assertEqual(Sum_Node.value,sum_list[i])
-            Sum_Node=Sum_Node.next
-
-
-if __name__ == '__main__':
-    try:
-        unittest.main()
-    except: 
-        SystemExit
-    input()
