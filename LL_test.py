66a66a90a585c270cc08665fda35af78db652b32
Dmitrii_56 <ilin.dmitrii1984@gmail.com>
2018-12-22 18:15:03 +0300

    Переделал тесты : теперь сравниваются значения списка List с значениями value , соответствующих узлов

--- LL_test.py
+++ LL_test.py
@@ -12,79 +12,67 @@ class Linked_List_2_Tests(unittest.TestCase):
     def test_insert_in_empty_list(self):
         # Проверка ф-ции insert при пустом списке
         LL_1=LL_mod.LinkedList()
-        LL_2=LL_mod.LinkedList()
         i=random_number()
         Node=Node_create(i)
-        LL_1.add_in_tail(Node)
-        if LL_2.len()==0:
-            LL_2.insert(None,i)
+        if LL_1.len()==0:
+            LL_1.insert(None,i)
         Node_LL_1=LL_1.head
-        Node_LL_2=LL_2.head
+        self.assertEqual(LL_1.head.value,i)
         for j in range (0,LL_1.len()):
-            self.assertEqual(Node_LL_1.value,Node_LL_2.value)
+            self.assertEqual(Node_LL_1.value,i)
 
     def test_insert_with_one_element(self):
         #Проверка ф-ции insert для списка с 1 элементом. Вставка э-та после первого
-        #э-та, повторная вставка и далее вставка после изначально существующего эл-та. 
+        #э-та, далее вставка после предварительно вставленного эл-та. 
         LL_1=LL_mod.LinkedList()
-        LL_2=LL_mod.LinkedList()
-        i=random_number()
-        j=random_number()
-        LL_1.add_in_tail(Node_create(i))
-        LL_2.insert(None,i)
+        Q_ty=3
+        data=[]
+        for i in range (0,Q_ty):
+            j=random_number()
+            data.append(j)
+        LL_1.add_in_tail(Node_create(data[0]))
+        LL_1.insert(data[0],data[1])
+        LL_1.insert(data[1],data[2])
         Node_LL_1=LL_1.head
-        Node_LL_2=LL_2.head
-        for j in range (0,LL_1.len()):
-            self.assertEqual(Node_LL_1.value,Node_LL_2.value)
-        LL_1.add_in_tail(Node_create(j))
-        LL_2.insert(i,j)
-        Node_LL_1=LL_1.head
-        Node_LL_2=LL_2.head
-        for j in range (0,LL_1.len()):
-            self.assertEqual(Node_LL_1.value,Node_LL_2.value)
+        for i in range (0,LL_1.len()):
+            self.assertEqual(Node_LL_1.value,data[i])
+            Node_LL_1=Node_LL_1.next
+
 
     def test_insert_in_last_position(self):
         #Вставка в конец списка
         LL_1=LL_mod.LinkedList()
-        LL_2=LL_mod.LinkedList()
         data=[]
         for i in range(0,10):
             data.append(random_number())
         for i in range(0,10):
             RN=random_number()
             if i==9:
-                LL_1.add_in_tail(Node_create(RN))
-                LL_2.insert(PN,RN)
+                LL_1.insert(data[i],RN)
+                data.append(i)
             else:
-                LL_1.add_in_tail(Node_create(RN))
-                LL_2.add_in_tail(Node_create(RN))
-            PN=RN
+                LL_1.add_in_tail(Node_create(data[i]))
         Node_LL_1=LL_1.head
-        Node_LL_2=LL_2.head
-        for i in range(0,10):
-            self.assertEqual(Node_LL_1.value,Node_LL_2.value)
+        for i in range(0,LL_1.len()):
+            self.assertEqual(Node_LL_1.value,data[i])
+            Node_LL_1=Node_LL_1.next
 
     def test_insert_in_random_position(self):
-       #Вставка в случайную позицию
+       #Вставка в случайную позицию, случайного значения
         LL_1=LL_mod.LinkedList()
-        LL_2=LL_mod.LinkedList()
         data=[]
         random_position=random.randint(1,8)
+        random_value=random.randint(1,100)
         for i in range(0,10):
-            data.append(random_number())
-        for i in range(0,10):
-            RN=data[i]
-            if i==random_position:
-                LL_1.add_in_tail(Node_create(RN))
-                LL_2.insert(PN,RN)
-            else:
-                LL_1.add_in_tail(Node_create(RN))
-                LL_2.add_in_tail(Node_create(RN))
-            PN=RN
+            data.append(random_number())    
+        for i in range(0,len(data)):
+            LL_1.add_in_tail(Node_create(data[i]))
+        data.insert(random_position,random_value)
+        LL_1.insert(data[random_position-1],random_value)    
         Node_LL_1=LL_1.head
-        Node_LL_2=LL_2.head
-        for i in range(0,10):
-            self.assertEqual(Node_LL_1.value,Node_LL_2.value)    
+        for i in range(0,LL_1.len()):
+            self.assertEqual(Node_LL_1.value,data[i])
+            Node_LL_1=Node_LL_1.next    
 
         
 
